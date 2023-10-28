4b77f305e570d9ad5070c22468662f3fa11a5fb2
Tejaswini Uma Sudhir <119094222+tejucodes10@users.noreply.github.com>
2023-10-28 14:29:56 +0530

    Add files via upload

---
+++ EX 4/src/Components/QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "Who is the prime minister of the country? ",
+        options: ["Amitabh Bachchan", "Sania Mirza", "Narendra Modi", "Mayavathi"],
+        answer: "Narendra Modi",
+    },
+    {
+        id: 2,
+        question: "Who is the heroine in the movie Raazi? ",
+        options: ["Mamatha Bannerjee", "Alia Bhatt", "Jaya Bachhan", "Barkha Dutt"],
+        answer: "Alia Bhatt",
+    },
+    {
+        id: 3,
+        question: "Who is called 'Amma' in Tamil Nadu?",
+        options: ["Jayalalitha", "Kiran Bedi", "Nayanthara", "PT Usha"],
+        answer: "Jayalalitha"
+    },
+    {
+        id: 4,
+        question: "Where does SRK live?",
+        options: ["Mannat", "Haridwar", "Rushikesh", "Dadra ki Haveli"],
+        answer: "Mannat"
+    },
+    {
+        id: 5,
+        question: "Who is the CEO of Google",
+        options: ["Yogi Adityanath", "Samantha", "Sundar Pichai", "Raveesh Kumar"],
+        answer: "Sundar Pichai"
+    },
+]
+ 
+export default qBank;
